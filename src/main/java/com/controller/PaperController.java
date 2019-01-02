f4e008c889601d762dc0f99a51e6c419f26c83f5
zhaoxiao <zxiao208@163.com>
2019-01-02 17:29:50 +0800

    1、增加json接口

--- src/main/java/com/controller/PaperController.java
+++ src/main/java/com/controller/PaperController.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
 @RequestMapping("/paper")
 public class PaperController {
 
+    @Autowired
+    private HttpServletRequest request;
     @Autowired
     private PaperService paperService;
 
@@ -40,7 +45,7 @@ public class PaperController {
         return "redirect:/paper/allPaper";
     }
 
-    @RequestMapping("toUpdatePaper")
+    @RequestMapping("/toUpdatePaper")
     public String toUpdatePaper(Model model, Long id) {
         model.addAttribute("paper", paperService.queryById(id));
         return "updatePaper";
@@ -52,6 +57,15 @@ public class PaperController {
         paper = paperService.queryById(paper.getPaperId());
         model.addAttribute("paper", paper);
         return "redirect:/paper/allPaper";
+    }
+
+    @RequestMapping(value = "/phone/addPaper",produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public String phoneaddPaper(@RequestBody String a){
+        String header1=request.getHeader("Connection");
+        System.out.println(header1);
+        System.out.println(a);
+        return a;
     }
 
 }
